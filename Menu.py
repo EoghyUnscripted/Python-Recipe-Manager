1a831042cbcfb15e93cdbc01e6952a3d6c0dc1fd
Eoghan <71570133+EoghyUnscripted@users.noreply.github.com>
2023-10-22 19:45:07 -0700

    Create Menu class file

---
+++ Menu.py
@@ -0,0 +1,23 @@
+from Classes.Recipe_Book import Book
+
+class Menu:
+    """Class object to store menu data for recipes."""
+    
+    def __init__(self):
+        self.bookshelf = []
+
+    def print_menu(self):
+        """Method to print menu to console."""
+        
+        pass
+
+    def prompt(self):
+        """Prompts user to input recipe book details."""
+        
+        # TODO: For loop to run program
+        
+        # TODO: Print the menu options
+        
+        # TODO: Get option number
+        
+        # TODO: Get recipe book menu
