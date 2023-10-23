2715e62a4704ce3df4417c3e023d3a26cb8bea46
Eoghan <71570133+EoghyUnscripted@users.noreply.github.com>
2023-10-22 19:43:42 -0700

    Update Recipe
    Removed getters & setters - not needed.
    Added new recipe methods and functions.

--- Classes/Modules/Recipe.py
+++ Classes/Modules/Recipe.py
@@ -1,143 +1,212 @@
-from Classes.Modules import Ingredient, Author
+from Author import Author
+from Diet import Diet
+from Ingredient import Ingredient
+from Instruction import Instruction
 
 class Recipe:
     """Class object to store recipe data."""
 
-    def __init__(self):
-        self.recipe_name = "Recipe Name"    # Recipe name
-        self.author_name = "Author" # Author object
-        self.prep_time = 0  # Recipe prep time
-        self.cook_time = 0  # Recipe cook time
-        self._total_time = 0 # Recipe total time
-        self.serving_size = 0   # Serving size
-        self.serving_cals = 0.0 # Individual serving calories
-        self._total_cals = 0.0   # Total recipe calories
-        self.ingredient_list = []   # List for ingredients
-        self.prep_list = [] # List for preparation steps
-        self.instruction_list = []  # List for cooking instructions
-        self.dietary_list = []    # List for dietary warnings
-
-    def get_recipe_name(self):
-        """Class method to return recipe name."""
+    def __init__(self, name="Sandwich", author="Author", prep=0.0, cook=0.0, servings=0,
+                 description = "Description", ingredients=[], instructions=[], dietary=[]):
         
-        return self.recipe_name # Return recipe name
+        ### User-populated ###
+        self.name = name
+        self.author = author
+        self.prep_time = prep   # In minutes
+        self.cook_time = cook   # In minutes
+        self.serving_size = servings
+        self.description = description
+        self.ingredient_list = ingredients
+        self.instruction_list = instructions
+        self.dietary_list = dietary    # List for dietary warnings
+        
+        ### Class-populated ###
+        self._total_time = float(self.prep_time) + float(self.cook_time)
+        self._ind_serving_cals = 0.0 # Individual serving calories
+        self._total_recipe_cals = 0.0
+        
+### Functions ###
+
+    def calculate_tot_time(self):
+        """Function to calculate total recipe time."""
+        
+        self._total_time = float(self.prep_time) + float(self.cook_time)
+
+    def calculate_tot_cals(self):
+        """Function to calculate total recipe calories by looping through ingredients list."""
+        
+        for i in self.ingredient_list:
+            
+            self._total_recipe_cals += i._total_ingredient_cals
+            
+    def calculate_single_cals(self):
+        """Function to calculate single serving calories."""
+        
+        self._ind_serving_cals = self._total_recipe_cals / float(self.serving_size)     # Divide total calories by serving count
     
-    def set_recipe_name(self, name):
-        """Class method to set recipe name."""
+### Methods ###
+
+    def populate_author(self):
+        """Method to create Author and set to recipe."""
+        
+        self.author = Author()
+        self.author.promt()
+
+    def populate_ingredients(self):
+        """Method to prompt user for ingredients list."""
+        
+        recipe_ingredients = []
+        
+        while True:
+            
+            next_ingredient = Ingredient()
+            next_ingredient.prompt()
+            
+            recipe_ingredients.append(next_ingredient)
+            
+            more_ingredients = input("Do you need more ingredients? (Y/N): ")
+
+            if more_ingredients.lower() == "y":
                 
-        self.recipe_name = name # Set recipe name
-
-    def get_author_name(self):
-        """Class method to return recipe author object to get name."""
-        
-        return self.author_name # Return author object
-
-    def set_author_name(self, author):
-        """Class method to update recipe author object with new author name."""
+                continue
+            
+            else:
                 
-        self.author = author    # Set updated Author object as author name
+                self.ingredient_list = recipe_ingredients                    
+                break
+            
+    def populate_instructions(self):
+        """Method to prompt user for instruction list."""
+        
+        recipe_instructions = []
+        
+        while True:
+            
+            next_instruction = Instruction()
+            next_instruction.prompt()
+            
+            recipe_instructions.append(next_instruction)
+            
+            more_instructions = input("Are there more instructions to add? (Y/N): ")
 
-    def get_prep_time(self):
-        """Class method to return recipe prep time."""
-        
-        return self.prep_time   # Return prep time
-    
-    def set_prep_time(self, time):
-        """Class method to set recipe prep time."""
-        
-        self.prep_time = time   # Return cook time
-    
-    def get_cook_time(self):
-        """Class method to return recipe cook time."""
-        
-        return self.cook_time   # Return cook time
-    
-    def set_cook_time(self, time):
-        """Class method to set recipe cook time."""
-        
-        self.cook_time = time   # Set cook time
- 
-    def get_total_time(self):
-        """Class method to return total time to create recipe."""
-        
-        return self.total_time   # Return total cook time
-    
-    def set_total_time(self):
-        """Class method to calculate and set total time to create recipe."""
+            if more_instructions.lower() == "y":
                 
-        self.total_time = self.prep_time + self.cook_time   # Calculate and set total recipe time
+                continue
+            
+            else:
+                
+                self.instruction_list = recipe_instructions                 
+                break
 
-    def get_serving_size(self):
-        """Class method to return individual serving size."""
+    def populate_dietary(self):
+        """Method to prompt user for dietary list."""
         
-        return self.serving_size   # Return serving size
+        recipe_dietary = []
+        
+        while True:
+            
+            next_dietary = Diet()
+            next_dietary.prompt()
+            
+            recipe_dietary.append(next_dietary)
+            
+            more_dietary = input("Are there more dietary warnings to add? (Y/N): ")
+
+            if more_dietary.lower() == "y":
+                
+                continue
+            
+            else:
+                
+                self.dietary_list = recipe_dietary                 
+                break
+
+    def print_summary(self):
+        """Method to print formatted recipe summary to console."""
+
+        print(
+              f"\n{self.name}\n"
+            + f"By: {self.author.full_name}\n"
+            + f"\n"
+            + f"Recipe Time: {self._total_time} minutes\n"
+            + f"Total Calories: {self._total_recipe_cals}\n"
+            + f"{self.description}\n"
+        )
     
-    def set_serving_size(self, size):
-        """Class method to set individual serving size."""
+    def print_recipe(self):
+        """Method to print formatted recipe to console."""
         
-        self.serving_size = size   # Set individual serving size
-    
-    def get_serving_cals(self):
-        """Class method to return individual serving calories."""
+        # Print recipe details
+        print(
+              f"\n{self.name}\n"
+            + f"By: {self.author.full_name}\n"
+            + f"\n"
+            + f"{self.description}\n"
+            + f"\n"
+            + f"Preparation Time: {self.prep_time} minutes\n"
+            + f"Cook Time: {self.prep_time} minutes\n"
+            + f"Yields: {self.serving_size} servings\n"
+            + f"Single Serving Calories: {self._ind_serving_cals}\n"
+            + f"Total Calories: {self._total_recipe_cals}\n"
+        )
         
-        return self.serving_cals    # Return single serving calories
-    
-    def set_serving_cals(self):
-        """Class method to set individual serving calories."""
+        # Print recipe ingredients if exists
+        if len(self.ingredient_list) > 0:
+            
+            print("Ingredients\n")
+            for i in self.ingredient_list:
+                
+                i.print_ingredient()
         
-        self.serving_cals = self.total_cals / self.serving_size # Set individual serving calories
-    
-    def get_total_cals(self):
-        """Class method to return total recipe calories."""
+        # Print recipe instructions if exists
+        if len(self.instruction_list) > 0:
+            
+            print("\nInstructions\n")
+            for i in self.instruction_list:
+                
+                i.print_instruction()
+            
+        # Print recipe dietary warnings if exists
+        if len(self.dietary_list) > 0:
+            
+            print("\nDietary\n")
+            for i in self.dietary_list:
+                
+                i.print_dietary()
+            print("\n")
         
-        return self.total_cals  # Return total recipe calories
-    
-    def set_total_cals(self, calories):
-        """Class method to set total recipe calories."""
+### Main ###
+
+    def prompt(self):
+        """Prompts user to input recipe details."""
         
-        self.total_cals = calories  # Set new total calories
+        while True:
+            
+            # Set Recipe details
+            self.name = input("What should we call this recipe?: ")
+            self.prep_time = input("How long does it take to prepare this recipe in minutes? (i.e. 2.5): ")   # In minutes
+            self.cook_time = input("How long does it take to cook this recipe in minutes? (i.e. 12.5): ")   # In minutes
+            self.serving_size = input("How many servings in this recipe?: ")
+            self.description = input("Please add a short description of this recipe:\n")
+            
+            # Call methods to populate Author, Ingredients, Instructions, and Dietary
+            self.populate_author()
+            self.populate_ingredients()
+            self.populate_instructions()
+            self.populate_dietary()
+            
+            # Call functions to calculate total recipe calories, time, and individual serving calories
+            self.calculate_tot_time()
+            self.calculate_tot_cals()
+            self.calculate_single_cals()
         
-    def calc_total_cals(self):
-        """Class method to calculate and set total recipe calories."""
-        
-        self.total_cals = self.serving_size * self.serving_cals  # Calculate and set total calories
-    
-    def get_ingredient_list(self):
-        """Class method to return ingredients list."""
-        
-        return self.ingredient_list   # Return ingredients list
-    
-    def set_ingredient_list(self, ingredients):
-        """Class method to set ingredients list."""
-        
-        self.ingredient_list = ingredients   # Set ingredients list
-    
-    def get_prep_list(self):
-        """Class method to return preparation steps list."""
-        
-        return self.prep_list   # Return preparation steps list
-    
-    def set_prep_list(self, preparations):
-        """Class method to set preparation steps list."""
-        
-        self.prep_list = preparations   # Set preparation steps list
-    
-    def get_instructions_list(self):
-        """Class method to return instructions list."""
-        
-        return self.instruction_list   # Return instructions list
-    
-    def set_instructions_list(self, instructions):
-        """Class method to set instructions list."""
-        
-        self.instruction_list = instructions   # Set instructions list
-    
-    def get_dietary_list(self):
-        """Class method to return dietary warnings list."""
-        
-        return self.dietary_list   # Return dietary warnings list
-    
-    def set_dietary_list(self, dietary):
-        """Class method to set dietary warnings list."""
-        
-        self.dietary_list = dietary   # Set dietary warnings list
+            # Verify recipe details with user
+            print("Here is your new recipe: \n")
+            print(50 * "*")
+            self.print_recipe()
+            print(50 * "*")
+            looks_good = input(f"\nDoes this look good? (Y/N): ")
+
+            if looks_good.lower() == "y":
+                
+                break
